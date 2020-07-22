d108a3e41c6e2bafa4d9f6c44c3cb2b98ac59c2d
Glitch (gorgeous-arrow-quarter) <none>
2020-07-22 09:18:58 +0000

    🌓🐽 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler"); // expressでもasync使いたい
+const app = express();
+const db = require("./sqlite.js");
+
+// 表示関連
+const HEIGHT = 28;
+const WIDTH = 27;
+const DIGIT = 8; //桁
+
+// 0埋め
+function zeroPadding(num) {
+  return ("0000000000" + num).slice(-DIGIT);
+}
+
+// SVG生成
+function makeSvg(viewer) {
+  let len = "";
+  const DISPLAY_NUM = zeroPadding(viewer);
+
+  for (let i = 0; i < DIGIT; i++) {
+    const IMG = `<rect id="Rectangle" fill="#000000" x=\"${i *
+      WIDTH}\" y="0" width=\"${WIDTH}\" height=\"${HEIGHT}\"></rect>
+      <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill="white">
+        <tspan x=\"${i * WIDTH + 6}\" y="22">${DISPLAY_NUM[i]}</tspan>
+      </text>`;
+
+    len = len + IMG;
+  }
+
+  return `<svg width=\"${WIDTH *
+    DIGIT}px\" height=\"${HEIGHT}px\" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>Counter</title>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+     ${len}
+    </g>
+  </svg>`;
+}
+
+// 簡易的な生死確認
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running</h1>");
+});
+
+// 閲覧回数返却
+app.get("/counter.svg",asyncHandler(async (req, res) => {
+    const VIEWER = (await db.read()) + 1; //　閲覧数読み取り
+    await db.add(VIEWER); // 更新
+    res.set({
+      "content-type": "image/svg+xml",
+      "cache-control": "max-age=0, no-cache, no-store, must-revalidate"
+    });
+    res.send(makeSvg(VIEWER));
+  })
+);
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
---
+++ sqlite.js
@@ -0,0 +1,55 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("counters.sqlite");
+
+db.serialize(() => {
+  let isViewer = false;
+  // テーブルがなければ作成
+  db.run("CREATE TABLE IF NOT EXISTS counter(id INTEGER , viewer INTEGER)");
+  
+  db.get('SELECT * FROM counter WHERE id = 0', function(err, row) {
+    isViewer = row.viewer ? true : false;
+    console.log(`row.id=>${row.id}, row.viewer=>${row.viewer}`);
+  });
+
+  // プリペアードステートメントでデータ挿入
+  if(!isViewer) {
+    db.run(`INSERT INTO counter values(0, 0)`, function(err) {
+      if (err) {
+        return console.error(err.message);
+      }
+    });
+  }
+});
+
+// 閲覧回数更新
+async function add(num) {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      try {
+        db.run(`UPDATE counter SET viewer = ${num} WHERE id = 0`);
+        return resolve();
+      } catch(e) {        
+        return reject(e);
+      }
+    });
+  });
+}
+
+// 閲覧回数取得
+async function read() {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      try {
+        let viewer = 0;
+        db.get('SELECT * FROM counter WHERE id = 0', (err, row) => {
+            console.log(row.viewer);
+            return resolve(row.viewer);
+        });
+      } catch(e) {        
+        return reject(e);
+      }
+    });
+  });
+}
+
+module.exports = { add, read };
