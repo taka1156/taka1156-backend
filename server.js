087f4427d8f061b5de7ee60070534713374ca370
taka <tomo05061@gmail.com>
2020-07-22 22:06:50 +0900

    fix: 処理の見直し

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const asyncHandler = require("express-async-handler"); // expressでもasync使いたい
+const express = require('express');
+const asyncHandler = require('express-async-handler'); // expressでもasync使いたい
 const app = express();
-const db = require("./sqlite.js");
+const db = require('./sqlite.js');
 
 // 表示関連
 const HEIGHT = 28;
@@ -10,17 +10,16 @@ const DIGIT = 8; //桁
 
 // 0埋め
 function zeroPadding(num) {
-  return ("0000000000" + num).slice(-DIGIT);
+  return ('0000000000' + num).slice(-DIGIT);
 }
 
 // SVG生成
 function makeSvg(viewer) {
-  let len = "";
+  let len = '';
   const DISPLAY_NUM = zeroPadding(viewer);
 
   for (let i = 0; i < DIGIT; i++) {
-    const IMG = `<rect id="Rectangle" fill="#000000" x=\"${i *
-      WIDTH}\" y="0" width=\"${WIDTH}\" height=\"${HEIGHT}\"></rect>
+    const IMG = `<rect id="Rectangle" fill="#000000" x=\"${i * WIDTH}\" y="0" width=\"${WIDTH}\" height=\"${HEIGHT}\"></rect>
       <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill="white">
         <tspan x=\"${i * WIDTH + 6}\" y="22">${DISPLAY_NUM[i]}</tspan>
       </text>`;
@@ -28,8 +27,7 @@ function makeSvg(viewer) {
     len = len + IMG;
   }
 
-  return `<svg width=\"${WIDTH *
-    DIGIT}px\" height=\"${HEIGHT}px\" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  return `<svg width=\"${WIDTH * DIGIT}px\" height=\"${HEIGHT}px\" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <title>Counter</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
      ${len}
@@ -38,22 +36,22 @@ function makeSvg(viewer) {
 }
 
 // 簡易的な生死確認
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const client = req.query.client
   if (client === 'gas') {
     res.send('Glitch woke up')
     return
   }
-  res.send("<h1>Server is running</h1>");
+  res.send('<h1>Server is running</h1>');
 });
 
 // 閲覧回数返却
-app.get("/counter.svg",asyncHandler(async (req, res) => {
+app.get('/counter.svg',asyncHandler(async (req, res) => {
     const VIEWER = (await db.read()) + 1; //　閲覧数読み取り
     await db.add(VIEWER); // 更新
     res.set({
-      "content-type": "image/svg+xml",
-      "cache-control": "max-age=0, no-cache, no-store, must-revalidate"
+      'content-type': 'image/svg+xml',
+      'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'
     });
     res.send(makeSvg(VIEWER));
   })
@@ -61,5 +59,5 @@ app.get("/counter.svg",asyncHandler(async (req, res) => {
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port' + listener.address().port);
 });
