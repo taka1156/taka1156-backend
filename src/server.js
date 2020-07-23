b377dcd9718e146e9aa0900f225e1f60a67450be
taka <tomo05061@gmail.com>
2020-07-23 11:33:08 +0900

    add: 日付の返却API追加

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const asyncHandler = require('express-async-handler'); // expressでもasync使いたい
 const app = express();
 const db = require('./tools/db/sqlite.js');
-const { generateSvg } = require('./tools/svg/svgGen.js')
+const { generateVisiterSvg, generateDateSvg } = require('./tools/svg/svgGen.js')
 
 // 簡易的な生死確認
 app.get('/', (req, res) => {
@@ -14,22 +14,32 @@ app.get('/', (req, res) => {
   res.send('<h1>Server is running</h1>');
 });
 
-// 閲覧回数返却
-app.get('/counter.svg',asyncHandler(async (req, res) => {
+// 画像返却(閲覧回数)
+app.get('/counter.svg', asyncHandler(async (req, res) => {
     const VIEWER = (await db.read()) + 1; //　閲覧数読み取り
     await db.add(VIEWER); // 更新
     res.set({
       'content-type': 'image/svg+xml',
       'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'
     });
-    res.send(generateSvg(VIEWER));
-  })
-);
+    res.send(generateVisiterSvg(VIEWER));
+}));
+
+// 画像返却(日付)
+app.get('/date.svg', asyncHandler(async (req, res) => {
+  const FORMAT_DATE = new Date(new Date()).toLocaleDateString()
+  res.set({
+    'content-type': 'image/svg+xml',
+    'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'
+  });
+  res.send(generateDateSvg(FORMAT_DATE));
+}));
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log('=============== Routers ===============')
   console.log(`[ http://localhost:${listener.address().port} ]`);
   console.log(`[ http://localhost:${listener.address().port}/counter.svg ]`);
+  console.log(`[ http://localhost:${listener.address().port}/date.svg ]`);
   console.log('======================================')
 });
--- src/tools/svg/svgGen.js
+++ src/tools/svg/svgGen.js
@@ -2,20 +2,20 @@
 const HEIGHT = 28;
 const WIDTH = 27;
 const DIGIT = 8; //桁
+const COLOR = 'cornflowerblue'
 
 // 0埋め
 function zeroPadding(num) {
   return ('0000000000' + num).slice(-DIGIT);
 }
 
-// SVG生成
-function generateSvg(viewer) {
-  // 表示関連
-  const FOMAT_NUM = zeroPadding(viewer).split('');
+// SVG生成(閲覧者数)
+function generateVisiterSvg(viewer) {
+  const FOMAT_NUM = zeroPadding(viewer);
 
-  const DISPLAY_NUM = FOMAT_NUM.reduce((accum, num, i) => {
+  const DISPLAY_NUM = FOMAT_NUM.split('').reduce((accum, num, i) => {
     return (accum + `<rect id="Rectangle" fill="#000000" x=\"${ i * WIDTH }\" y="0" width=\"${ WIDTH }\" height=\"${ HEIGHT }\"></rect>
-        <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill="white">
+        <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill=\"${ COLOR }\">
             <tspan x=\"${ i * WIDTH + 6 }\" y="22">${ num }</tspan>
         </text>`
     );
@@ -29,4 +29,24 @@ function generateSvg(viewer) {
     </svg>`;
 }
 
-module.exports = { generateSvg }
+// SVG生成(日付)
+function generateDateSvg(date) {
+
+    const DISPLAY_DATE = date.split('').reduce((accum, char, i) => {
+      let outChar  = char === '-' ? '/': char;
+      return (accum + `<rect id="Rectangle" fill="#000000" x=\"${ i * WIDTH }\" y="0" width=\"${ WIDTH }\" height=\"${ HEIGHT }\"></rect>
+          <text id="0" font-family="Courier" font-size="24" font-weight="normal" fill=\"${ COLOR }\">
+              <tspan x=\"${ i * WIDTH + 6 }\" y="22">${ outChar }</tspan>
+          </text>`
+      );
+    }, '');
+  
+    return `<svg width=\"${ WIDTH * DIGIT }px\" height=\"${ HEIGHT }px\" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <title>Counter</title>
+          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          ${ DISPLAY_DATE }
+          </g>
+      </svg>`;
+  }
+
+module.exports = { generateVisiterSvg, generateDateSvg }
