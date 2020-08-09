d21be0c11b8915c913b33b37ea0708248f25325c
taka <tomo05061@gmail.com>
2020-08-09 18:58:07 +0900

    update: プロジェクtのフォルダ構成を見直し

---
+++ src/middleware/visiter/visiter.js
@@ -0,0 +1,11 @@
+const db = require('../../tools/db/sqlite.js');
+const { generateVisiterSvg } = require('../../tools/svg/svgGen.js');
+
+// 閲覧者を計算して返却
+async function getVisiterCounts() {
+  const VIEWER = (await db.read()) + 1; //　閲覧数読み取り
+  await db.add(VIEWER); // 更新
+  return generateVisiterSvg(VIEWER);
+}
+
+module.exports = { getVisiterCounts };
