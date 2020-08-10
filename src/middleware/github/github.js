3f47653982915ec113e52c40da8edeb14c251c35
taka <tomo05061@gmail.com>
2020-08-11 00:10:12 +0900

    update: クエリ追加、github APIへの問い合わせをGraphQLに変更

--- src/middleware/github/github.js
+++ src/middleware/github/github.js
@@ -3,44 +3,109 @@ const axios = require('axios');
 require('dotenv').config();
 
 // リポジトリデータを、Github APIから取得してSVGに変換して返却
-async function getGithubRepos() {
-  const GITHUB_ACCOUNT = process.env.GITHUB_ACCOUNT;
-  const GITHUB_API = `https://api.github.com/users/${GITHUB_ACCOUNT}/repos?per_page=100&page=1`;
+async function getGithubRepos(account = 'taka1156') {
+  const GITHUB_API = `https://api.github.com/graphql`;
+  const QUERY = graphQLQuery(account);
+  const repos = await axios({
+    headers: { Authorization: `bearer ${process.env.GITHUB_TOKEN}` },
+    url: GITHUB_API,
+    method: 'post',
+    data: QUERY,
+  })
+    .then(({ data }) => {
+      let result = Array.from(data.data.user.repositories.nodes);
+      result = result.filter((repo) => repo.languages.edges.length !== 0);
+      result = result.map((repo) => repo.languages);
+      return result;
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
 
-  const { data } = await axios.get(GITHUB_API).catch((e) => {
-    console.log(e.message);
-  });
-
-  const REPOS = ShapedData(data)
+  const REPOS = ShapedData(repos);
   return generateGithubSvg(REPOS);
 }
 
 // リポジトリデータを整形
 function ShapedData(repos) {
-  let repo_langs = [];
-
+  // 各リポジトリの情報を{言語、イメージカラー、サイズ}オブジェクトの配列にする
+  let langs = [];
   repos.forEach((repo) => {
-    if (repo.language !== null) {
-      repo_langs.push(repo.language);
+    repo.edges.forEach((lang) => {
+      const { size, node } = lang;
+      langs.push({
+        name: node.name,
+        color: node.color,
+        size: size,
+      });
+    });
+  });
+
+  // それぞれを単一の配列に整形
+  const LANGS_NAME = [...new Set([...langs.map((lang) => lang.name)])];
+  const LANGS_COLOR = [...new Set([...langs.map((lang) => lang.color)])];
+  // 言語ごとの合計ファイルサイズの配列を作り初期化
+  const LANGS_TOTAL_SIZE = new Array(LANGS_NAME.length).fill(0); // 言語ごとの合計データ数を算出
+
+  LANGS_NAME.forEach((lang, index) => {
+    for (let i = 0, max = langs.length; i < max; i++) {
+      if (langs[i].name === lang) {
+        LANGS_TOTAL_SIZE[index] += langs[i].size;
+      }
     }
   });
 
-  const LANG_LIST = [...new Set(repo_langs)];
+  // 整形した各データを結合(sortした際に崩れるため、この時点で、colorも結合する)
+  const REPOS_DATA = LANGS_NAME.map((lang, index) => {
+    return {
+      name: lang,
+      color: LANGS_COLOR[index],
+      size: LANGS_TOTAL_SIZE[index],
+    };
+  });
 
-  let graph = [];
-  for (let i = 0; i < LANG_LIST.length; i++) {
-    let count = 0;
-    const LANG = LANG_LIST[i];
-    repo_langs.forEach((lang) => {
-      if (LANG === lang) {
-        count++;
+  // サイズの大きい順にソート
+  REPOS_DATA.sort((a, b) => b.size - a.size);
+
+  // トップ5件のみ切り出し
+  const TOP_USED_LANGS = REPOS_DATA.slice(0, 5);
+  // トップ5件の合計サイズ
+  const TOTAL_SIZE = TOP_USED_LANGS.reduce((accum, repo) => (accum += repo.size), 0);
+
+  // トップ5件の総データサイズを母数とした割合を出す
+  const RESULT = TOP_USED_LANGS.map((lang) => {
+    return {
+      name: lang.name,
+      color: lang.color,
+      rate: Math.round((lang.size / TOTAL_SIZE) * 10000) / 100,
+    };
+  });
+
+  console.log(RESULT);
+
+  return RESULT;
+}
+
+function graphQLQuery(account) {
+  return {
+    query: `query {
+    user(login: "${account}") {
+      repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+        nodes {
+          languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+            edges {
+              size
+              node {
+                color
+                name
+              } 
+            }
+          }
+        }
       }
-    });
-    const RATE = Math.ceil((count / repo_langs.length) * 100);
-    graph.push({ lang: LANG, rate: RATE });
-  }
-
-  return graph;
+    }
+  }`,
+  };
 }
 
 module.exports = { getGithubRepos };
--- src/middleware/visiter/visiter.js
+++ src/middleware/visiter/visiter.js
@@ -2,10 +2,10 @@ const db = require('../../tools/db/sqlite.js');
 const { generateVisiterSvg } = require('../../tools/svg/svgGen.js');
 
 // 閲覧者を計算して返却
-async function getVisiterCounts() {
+async function getVisiterCounts(color) {
   const VIEWER = (await db.read()) + 1; //　閲覧数読み取り
   await db.add(VIEWER); // 更新
-  return generateVisiterSvg(VIEWER);
+  return generateVisiterSvg(VIEWER, color);
 }
 
 module.exports = { getVisiterCounts };
